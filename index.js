e63fbcd6faec4b5da5d9385c943752979f5fb59a
peter <mutisyapeter336@gmail.com>
2024-09-29 22:37:30 +0300

    added a new file

---
+++ index.js
@@ -0,0 +1,19 @@
+// question 1 solution
+  
+let mark = 60
+
+if (mark >= 79) {
+  console.log('A')
+}else if ( mark >= 60) {
+  console.log('B')
+}else if (mark >= 49){
+  console.log('C')
+}else if (mark >= 40){
+  console.log('D')
+}else{
+  (console.log('E')
+  )
+}
+
+
+/// Question 2
